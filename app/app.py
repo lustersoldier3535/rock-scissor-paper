0ebe412c13d2a09326519f86dc99069e8b20d608
lustersoldier3535 <136288708+lustersoldier3535@users.noreply.github.com>
2023-06-12 05:01:33 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -22,7 +22,21 @@ user_label.grid(row=1, column=4)
 
 
 #scores
+playerScore = Label(root, text=0 ,font=100, bg="#9b59b6", fg="white")
+computerScore = Label(root, text=0, font=100, bg="#9b59b6", fg="white")
+computerScore.grid(row=1, column=1)
+playerScore.grid(row=1, column=3)
 
+#indicators
+user_indicator = Label(root, font=50, text="PLAYER")
+comp_indicater = Label(root, font=50, text="COMPUTER")
+user_indicator.grid(row=0, column=3)
+comuter_indicator.grid(row=0, column=1)
+
+#buttons
+rock = Button(root, width=20, height=2, text="ROCK", bg="#FFFFFF", fg="white").grid(row=2, column=1)
+paper = Button(root, width=20, height=2, text="PAPER", bg="#FFFFFFF", fg="white").grid(row=2, column=2)
+scissor = Button(root, width=20, height=2, text="SCISSOR", bg="#FFFFFF", fg="white").grid(row=2, column=3)
 
 
 root.mainloop() 
